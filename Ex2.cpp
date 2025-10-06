cc12dc50dc29d7c7c18016a43045be477c243f6e
mac <emile.khoury1@net.usj.edu.lb>
2025-10-06 11:51:42 +0300

    Ex2: Error Message Bad Solution

--- Ex2.cpp
+++ Ex2.cpp
@@ -1,11 +1,19 @@
 #include <iostream> 
 using namespace std;
 int main() {
-int n; do{
-cout<<"Enter a positive integer less than 70 :"<<endl; cin>>n;
-}while(n<=0 || n>=70);
-for(int i=10; i>=0; i--)
-{
-cout<<n<<"x"<<i<<"="<<n*i<<endl;
-}
+    int n;
+    do{
+    cout<<"Enter an integer greater than 0 and less than 70: "<<endl; cin>>n;
+
+    if(n<=0) {
+    
+    cout<<"Your number is less than or equal to 0!"<<endl;
+    } 
+    if(n>=70) {
+    
+    cout<<"Your number is greater than or equal to 70"<<endl; }
+    }
+    while(n<=0 || n>=70);
+    for(int i=10; i>=0; i--) {
+    cout<<n<<"x"<<i<<"="<<n*i<<endl; }
 return 0; }
