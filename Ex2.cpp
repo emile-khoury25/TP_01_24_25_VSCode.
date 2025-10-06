39beee862fd59e3eb880e0c068de709041061e3e
mac <emile.khoury1@net.usj.edu.lb>
2025-10-06 11:50:28 +0300

    Implement Ex2, a positive integer between 0 and 70

--- Ex2.cpp
+++ Ex2.cpp
@@ -1,13 +1,11 @@
 #include <iostream> 
 using namespace std;
 int main() {
-int n; 
-do{
-
-    cout<<"Enter a positive integer:"<<endl; cin>>n;
-} while(n<=0);
-    for(int i=10; i>=0; i--) {
-    cout<<n<<"x"<<i<<"="<<n*i<<endl; }
-    return 0;
-
-    }
+int n; do{
+cout<<"Enter a positive integer less than 70 :"<<endl; cin>>n;
+}while(n<=0 || n>=70);
+for(int i=10; i>=0; i--)
+{
+cout<<n<<"x"<<i<<"="<<n*i<<endl;
+}
+return 0; }
