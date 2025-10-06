4c5e698f1899e1e794d5b26748de634c6242908e
mac <emile.khoury1@net.usj.edu.lb>
2025-10-06 11:47:16 +0300

    Implement Ex2

--- Ex2.cpp
+++ Ex2.cpp
@@ -1,6 +1,10 @@
 #include <iostream> 
 using namespace std;
 int main() {
-    cout<<"Ex2"<<endl; 
+    int n;
+cout<<"Enter a positive integer:"<<endl; cin>>n;
+for(int i=10; i>=0; i--)
+{
+cout<<n<<"x"<<i<<"="<<n*i<<endl; }
 return 0;
 }
